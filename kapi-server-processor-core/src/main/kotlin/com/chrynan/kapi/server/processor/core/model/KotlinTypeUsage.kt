f9b1d9fa36a6eea156ac869425676d88262e26a8
Chris Keenan <chrynan@pm.me>
2023-03-02 13:53:42 -0600

    Added convenience type check properties to KotlinTypeUsage

--- kapi-server-processor-core/src/main/kotlin/com/chrynan/kapi/server/processor/core/model/KotlinTypeUsage.kt
+++ kapi-server-processor-core/src/main/kotlin/com/chrynan/kapi/server/processor/core/model/KotlinTypeUsage.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package com.chrynan.kapi.server.processor.core.model
 
 import kotlinx.serialization.Serializable
@@ -20,3 +22,81 @@ data class KotlinTypeUsage(
     @SerialName(value = "nullable") val isNullable: Boolean = false,
     @SerialName(value = "annotations") val annotations: List<KotlinAnnotationUsage> = emptyList()
 )
+
+/**
+ * Determines if this type is a Kotlin Boolean.
+ */
+val KotlinTypeUsage.isBoolean: Boolean
+    get() = name.full == "kotlin.Boolean"
+
+/**
+ * Determines if this type is a Kotlin Byte.
+ */
+val KotlinTypeUsage.isByte: Boolean
+    get() = name.full == "kotlin.Byte"
+
+/**
+ * Determines if this type is a Kotlin Short.
+ */
+val KotlinTypeUsage.isShort: Boolean
+    get() = name.full == "kotlin.Short"
+
+/**
+ * Determines if this type is a Kotlin Int.
+ */
+val KotlinTypeUsage.isInt: Boolean
+    get() = name.full == "kotlin.Int"
+
+/**
+ * Determines if this type is a Kotlin Long.
+ */
+val KotlinTypeUsage.isLong: Boolean
+    get() = name.full == "kotlin.Long"
+
+/**
+ * Determines if this type is a Kotlin Float.
+ */
+val KotlinTypeUsage.isFloat: Boolean
+    get() = name.full == "kotlin.Float"
+
+/**
+ * Determines if this type is a Kotlin Double.
+ */
+val KotlinTypeUsage.isDouble: Boolean
+    get() = name.full == "kotlin.Double"
+
+/**
+ * Determines if this type is a Kotlin Char.
+ */
+val KotlinTypeUsage.isChar: Boolean
+    get() = name.full == "kotlin.Char"
+
+/**
+ * Determines if this type is a Kotlin String.
+ */
+val KotlinTypeUsage.isString: Boolean
+    get() = name.full == "kotlin.String"
+
+/**
+ * Determines if this type is a Kotlin Unit.
+ */
+val KotlinTypeUsage.isUnit: Boolean
+    get() = name.full == "kotlin.Unit"
+
+/**
+ * Determines if this type is a Kotlin Nothing.
+ */
+val KotlinTypeUsage.isNothing: Boolean
+    get() = name.full == "kotlin.Nothing"
+
+/**
+ * Determines if this type is a Kapi Response.
+ */
+val KotlinTypeUsage.isResponse: Boolean
+    get() = name.full == "com.chrynan.kapi.core.Response"
+
+/**
+ * Determines if this type is a Ktor HttpResponse.
+ */
+val KotlinTypeUsage.isHttpResponse: Boolean
+    get() = name.full == "io.ktor.client.statement.HttpResponse"
