dfea4b37e2086190545a4fe15e0a5a61a08f47ab
Chris Keenan <chrynan@pm.me>
2023-03-12 19:08:49 -0500

    Created Encoding class

---
+++ kapi-openapi/src/commonMain/kotlin/com/chrynan/kapi/openapi/Encoding.kt
@@ -0,0 +1,81 @@
+package com.chrynan.kapi.openapi
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * A single encoding definition applied to a single schema property.
+ *
+ * This object MAY be extended with
+ * [Specification Extensions](https://spec.openapis.org/oas/v3.1.0#specificationExtensions).
+ *
+ * ## Encoding Object Example
+ *
+ * ```yaml
+ * requestBody:
+ *   content:
+ *     multipart/form-data:
+ *       schema:
+ *         type: object
+ *         properties:
+ *           id:
+ *             # default is text/plain
+ *             type: string
+ *             format: uuid
+ *           address:
+ *             # default is application/json
+ *             type: object
+ *             properties: {}
+ *           historyMetadata:
+ *             # need to declare XML format!
+ *             description: metadata in XML format
+ *             type: object
+ *             properties: {}
+ *           profileImage: {}
+ *       encoding:
+ *         historyMetadata:
+ *           # require XML Content-Type in utf-8 encoding
+ *           contentType: application/xml; charset=utf-8
+ *         profileImage:
+ *           # only accept png/jpeg
+ *           contentType: image/png, image/jpeg
+ *           headers:
+ *             X-Rate-Limit-Limit:
+ *               description: The number of allowed requests in the current period
+ *               schema:
+ *                 type: integer
+ * ```
+ *
+ * @property [contentType] The Content-Type for encoding a specific property. Default value depends on the property
+ * type: for object - application/json; for array – the default is defined based on the inner type; for all other cases
+ * the default is application/octet-stream. The value can be a specific media type (e.g. application/json), a wildcard
+ * media type (e.g. image/\*), or a comma-separated list of the two types.
+ * @property [headers] A map allowing additional information to be provided as headers, for example
+ * Content-Disposition. Content-Type is described separately and SHALL be ignored in this section. This property SHALL
+ * be ignored if the request body media type is not a multipart.
+ * @property [style] Describes how a specific property value will be serialized depending on its type. See Parameter
+ * Object for details on the style property. The behavior follows the same values as query parameters, including
+ * default values. This property SHALL be ignored if the request body media type is not
+ * application/x-www-form-urlencoded or multipart/form-data. If a value is explicitly defined, then the value of
+ * contentType (implicit or explicit) SHALL be ignored.
+ * @property [explode] When this is true, property values of type array or object generate separate parameters for each
+ * value of the array, or key-value-pair of the map. For other types of properties this property has no effect. When
+ * style is form, the default value is true. For all other styles, the default value is false. This property SHALL be
+ * ignored if the request body media type is not application/x-www-form-urlencoded or multipart/form-data. If a value
+ * is explicitly defined, then the value of contentType (implicit or explicit) SHALL be ignored.
+ * @property [allowReserved] Determines whether the parameter value SHOULD allow reserved characters, as defined by
+ * [RFC3986](https://spec.openapis.org/oas/v3.1.0#bib-RFC3986) :/?#[]@!$&'()*+,;= to be included without
+ * percent-encoding. The default value is false. This property SHALL be ignored if the request body media type is not
+ * application/x-www-form-urlencoded or multipart/form-data. If a value is explicitly defined, then the value of
+ * contentType (implicit or explicit) SHALL be ignored.
+ *
+ * @see [OpenApi Specification](https://spec.openapis.org/oas/v3.1.0#encoding-object)
+ */
+@Serializable
+data class Encoding(
+    @SerialName(value = "contentType") val contentType: String? = null,
+    @SerialName(value = "headers") val headers: Map<String, ReferenceOrType<Header>>? = null,
+    @SerialName(value = "style") val style: String? = null,
+    @SerialName(value = "explode") val explode: Boolean = false,
+    @SerialName(value = "allowReserved") val allowReserved: Boolean = false
+)
