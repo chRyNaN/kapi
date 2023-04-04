acc232ad59581eaea715268e6b1231c10471164b
Chris Keenan <chrynan@pm.me>
2023-04-04 15:08:03 -0500

    Added project graph gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     id("com.android.library") version "7.3.1" apply false
     id("com.android.application") version "7.3.1" apply false
     id("org.jetbrains.dokka") version "1.7.20"
+    id("com.vanniktech.dependency.graph.generator") version "0.7.0"
 }
 
 allprojects {
