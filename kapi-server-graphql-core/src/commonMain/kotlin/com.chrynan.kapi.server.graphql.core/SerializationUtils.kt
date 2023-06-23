60e2e762e7b70a55a20b29c94bee67b2a82df0dc
Chris Keenan <chrynan@pm.me>
2023-06-22 20:55:18 -0500

    Created SerializationUtils

---
+++ kapi-server-graphql-core/src/commonMain/kotlin/com.chrynan.kapi.server.graphql.core/SerializationUtils.kt
@@ -0,0 +1,26 @@
+package com.chrynan.kapi.server.graphql.core
+
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
+
+internal fun <T> Json.encodeToGraphQLJson(
+    serializer: SerializationStrategy<T>,
+    value: T,
+    fieldNames: Set<String> = emptySet()
+): JsonElement {
+    val json = this.encodeToJsonElement(serializer = serializer, value = value)
+
+    return if (fieldNames.isNotEmpty() && json is JsonObject) {
+        buildJsonObject {
+            json.entries.filter { it.key in fieldNames }
+                .forEach {
+                    put(key = it.key, element = it.value)
+                }
+        }
+    } else {
+        json
+    }
+}
